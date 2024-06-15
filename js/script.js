e1f3d8c73aba694c3c8a35dbc65aa518d9e975c0
Richar Chinchilla Gómez <richarchinchillagomez@gmail.com>
2024-06-14 22:02:03 -0500

    finalizando las cards de price y arreglando el menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,28 @@
 const headerMenuBurger = document.querySelector(".header__menuBurger");
 const headerMenuNav = document.querySelector(".header__menu--nav");
 
-headerMenuBurger.addEventListener("click",()=> {
-    headerMenuNav.classList.toggle("header__menu--nav--toggle");
+const headerContentInformation = document.querySelector(
+  ".header__content__information"
+);
+
+const headerLogo = document.querySelector(".header__logo");
+
+headerMenuBurger.addEventListener("click", () => {
+  headerMenuNav.classList.toggle("header__menu--nav--toggle");
+});
+
+addEventListener("scroll", (e) => {
+  if (window.scrollY > 100) {
+    headerContentInformation.classList.add(
+      "header__content__information--white"
+    );
+    headerMenuBurger.src = "../icons/menu_gris.svg";
+    headerLogo.classList.add("header__logo--gray");
+  } else {
+    headerContentInformation.classList.remove(
+      "header__content__information--white"
+    );
+    headerMenuBurger.src = "../icons/menu.svg";
+    headerLogo.classList.remove("header__logo--gray");
+  }
 });
