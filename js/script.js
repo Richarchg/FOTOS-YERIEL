d82166025f90aad7ecf52f1926ea461ba8d61ec5
Richar Chinchilla Gómez <richarchinchillagomez@gmail.com>
2024-06-17 20:53:37 -0500

    se está finalizando la página principal en pantallas grandes

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const headerMenuNav = document.querySelector(".header__menu--nav");
 const headerContentInformation = document.querySelector(".header__content__information");
 
 const headerLogo = document.querySelector(".header__logo");
+const headerMenuA = document.querySelectorAll(".header__menu--a");
 
 headerContentMenuBurger.addEventListener("click", () => {
   headerMenuNav.classList.toggle("header__menu--nav--toggle");
@@ -35,3 +36,19 @@ addEventListener("scroll", (e) => {
 
 let footerYear = document.querySelector(".footer__year");
 footerYear.textContent = new Date().getFullYear();
+
+addEventListener("DOMContentLoaded", () => {
+  addEventListener("scroll", () => {
+    headerMenuA.forEach(headerMenuA => {
+      if (window.innerWidth > 1024) {
+        if (window.scrollY > 100) {
+          // headerMenuA.classList.remove("header__menu--a");
+          headerMenuA.classList.add("header__menu--a--withHover");
+        } else {
+          headerMenuA.classList.remove("header__menu--a--withHover");
+          headerMenuA.classList.add("header__menu--a");
+        }
+      }
+    })
+  })
+});
