4c370a4e2bd21e1e08c0db55318ae5d3149885b9
Richar Chinchilla Gómez <richarchinchillagomez@gmail.com>
2024-06-29 21:08:09 -0500

    por fin se terminó la plantilla de galería de imagenes 29/06/2024

--- js/script__gallery.js
+++ js/script__gallery.js
@@ -12,5 +12,21 @@ arrowUp.addEventListener("click", () => {
   console.log(scroll(0, 0));
 });
 
+const imagenes = document.querySelectorAll(".main__section__photo");
+const containerLightbox = document.querySelector(".container__lightbox__disabled");
+const imagenLightbox = document.querySelector(".imagen__lightbox");
+
+imagenes.forEach(imagen => {
+  imagen.addEventListener("click", () => {
+    containerLightbox.classList.replace("container__lightbox__disabled", "container__lightbox__enabled")
+    imagenLightbox.src = imagen.src;
+  });
+  containerLightbox.addEventListener("click", () => {
+    containerLightbox.classList.replace("container__lightbox__enabled", "container__lightbox__disabled")
+  })
+});
+
+
+
 let footerYear = document.querySelector(".footer__year");
 footerYear.textContent = new Date().getFullYear();
